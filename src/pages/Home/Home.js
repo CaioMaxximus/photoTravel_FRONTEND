cfce717d54ac29db37af322ba66dacc7fefc3856
CaioMaxximus <caio.soares@ccc.ufcg.edu.br>
2020-07-17 00:35:10 -0300

    making first changes

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './style.css'
+const background = require('../../pictures/antoine-julien-r9QZo2VJx6w-unsplash.jpg')
+
+function Home() {
+    return <div>
+        <div id="background">
+            <img src={background} alt="background" />
+        </div>
+        <div class="content">
+            adasdadasda
+            dasdad
+            dadasda
+        </div>
+    </div>
+}
+
+export default Home
