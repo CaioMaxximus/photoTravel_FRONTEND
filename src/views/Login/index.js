a213bf4d09e317da132dac4f53ca3de9301d56d1
CaioMaxximus <caio.soares@ccc.ufcg.edu.br>
2021-08-09 01:05:04 -0300

    crating_new_views

---
+++ src/views/Login/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+export function Login(props) {
+
+
+    return (
+        <div id="login-screen">
+            <form className="form-user-area">
+                <label htmlFor="name">NickName or Email</label>
+                <input type="text" name="nick-email" />
+                <label htmlFor="password">Password</label>
+                <input type="text" name="password" />
+                <button>Login</button>
+            </form>
+
+        </div>)
+}
--- src/views/Register/index.js
+++ src/views/Register/index.js
@@ -1,23 +1,51 @@
 import { React, useState } from 'react'
+import api from './../../resources/api.js'
 
-export default function register(props) {
-
-
-    let [nickname, setNickname]; [email , setEmail];[description , setDescription] ;[password , setPassword] ; [passwordConfirm , setPasswordConfirm] = useState(""); 
+export default function Register(props) {
 
 
-    return <div id="register-screen">
-        <div class="form-user-area">
-            <div id = "in-first-step">
-                <label htmlFor="">NickName: </label><input type="text"  value= {nickname}  onChange = {setNickname}/>
-                <label htmlFor="">Email:</label><input type="text"  value= {email}  onChange = {setEmail}/>
-                <label htmlFor="">Password: </label><input type="text"  value= {password}  onChange = {setPassword}/>
-                <label htmlFor="">Confirm Password </label><input type="text"  value= {passwordConfirm}  onChange = {setPasswordConfirm} />
+    let [nickname, setNickname];[email, setEmail];[description, setDescription];[password, setPassword];[passwordConfirm, setPasswordConfirm] = useState("");
+
+
+
+    function registerUser() {
+
+        if (verifyData) {
+            api.post("users", {
+                nickname,
+                email,
+                description,
+                password
+            }).then(() => {
+                ///vai para a tela de posts!!
+            }).catch((e) => {
+                console.log(e);
+                ///exibe o erro para o user
+            })
+        }
+    }
+
+
+    return (
+        <div id="register-screen">
+            <div class="form-user-area">
+                <div id="in-first-step">
+                    <label htmlFor="nickname">NickName: </label>
+                    <input name = "nickname" type="text" value={nickname} onChange={setNickname} />
+
+                    <label htmlFor="email">Email:</label>
+                    <input name = "email" type="text" value={email} onChange={setEmail} />
+
+                    <label htmlFor="password">Password: </label>
+                    <input name = "password" type="text" value={password} onChange={setPassword} />
+
+                    <label htmlFor="confirm-password">Confirm Password </label>
+                    <input name = "confirm-password" type="text" value={passwordConfirm} onChange={setPasswordConfirm} />
+                </div>
+                <div id="in-second-step">
+                    <label htmlFor="description">Add a description about you: </label><textarea name="description" id="" cols="30" rows="10"></textarea>
+                </div>
+                <button onClick={() => { registerUser() }} >REGISTER</button>
             </div>
-            <div id = "in-second-step">
-                <label htmlFor="">Add a description about you: </label><textarea name="" id="" cols="30" rows="10"></textarea>
-            </div>
-            <button>REGISTER</button>
-        </div>
-    </div>
+        </div>)
 }
