74dc65ea16c7e532afda6391c764d1416b50de96
CaioMaxximus <caio.soares@ccc.ufcg.edu.br>
2021-08-12 14:39:26 -0300

    tag-div-system-working

--- src/Components/PostCreator/PostCreator.js
+++ src/Components/PostCreator/PostCreator.js
@@ -1,41 +1,51 @@
-import React, {useState} from  'react'
+import React, { useState } from 'react'
+import './style.css'
 
 
 
 /// a consertar de classes e id
-export default function PostCreator(){
+export default function PostCreator() {
 
 
     let [tags, setTags] = useState([]);
-    let [tagString , setTagString] = useState("");
+    let [tagString, setTagString] = useState("");
+    let [actualTag, setActualTag] = useState("");
 
-    function tagsMarker(e){
+    function tagsMarker(e) {
 
-        console.log(e.target.value)
-        setTagString(e.target.value);
-        let local_tags = tagString.split(" ");
-        let exit = []
-        for (var i = 0 ; i <= local_tags.length; i++){
-            exit.push(local_tags[i]);
+        ///talvez nao precise de useState em tags
+        let inp = e.target.value.split(" ");
+        let len = inp.length - 1;
+        setActualTag(inp[len])
+        console.log({inp})
+        console.log({tags })
+        if(inp.length >  1){
+            let local_tags = tags;
+            local_tags.push(inp[0]);
+            setTags(local_tags);
+        }
+        
 
-        } 
-        setTags(exit);
+        
     }
 
-    return (<div id = "post-creator">
-        <div className = "content">
-            <div id = "form">
+    return (<div id="post-creator">e
+
+        {/* trocar por form talvez */}
+        <div className="content">
+            <div id="form">
                 <label htmlFor="link">ImageLink:</label>
-                <input name = "link" type="text" />
-                <label htmlFor = "tags">Add some tags to you image..</label>
-                <textarea name="" id="" cols="30" rows="10" 
-                placeholder = "put each tag separated for space"
-                 onChange ={(e)=>
-                    tagsMarker(e)}
-                 value = {tagString}
-                ></textarea>
-                <div id = "tags-area">{tags.map(tag => <button className = "tag-miniature-creation">{tag}</button>)}</div>
+                <input name="link" type="text" />
+                <label htmlFor="tags">Add some tags to you image..</label>
+    
+                    <div id = "tags-creator">
+                        <div id="tags-area">{tags.map(tag => <button className="tag-miniature-creation">{tag}</button>)}</div>
+                        <input type="text" onChange={(e) =>
+                        tagsMarker(e)}
+                        value={actualTag}/>
+                    </div>
+                
             </div>
-        </div>
-    </div>)
+            </div>
+        </div>)
 }
