8219a64fb0110574ba18661322528e50b87be8db
CaioMaxximus <caio.soares@ccc.ufcg.edu.br>
2022-08-30 23:54:15 -0300

    fixing_like_button

--- src/Components/PostComplete/PostComplete.js
+++ src/Components/PostComplete/PostComplete.js
@@ -9,25 +9,50 @@ export default function PostComplete() {
 
     // const id = useLocation().pathname.split("/")[3];
     let { postId } = useParams();
-    console.log(postId, "id post")
 
     let [post, setPost] = useState([])
+    let [numLikes, setNumLikes] = useState(0)
 
     useEffect(() => {
+
+        const token = localStorage.getItem("user-local-token");
+
         api.get(`/posts/${postId}`).then((r) => {
             console.log(r)
             window.lista = r.data.tags
             setPost(r.data)
+            setNumLikes(r.data.numLikes)
         }).catch(e => { alert("erro 404") })
+
+
+        api.get(`/posts/likes/${postId}`,
+            { headers: { "Authorization": `Bearer ${token}` } }).
+            then((res) => {
+                let $btn = document.getElementById("post-like-btn")
+                $btn.style.backgroundColor = "blue";
+                console.log(res.data)
+
+            }).catch(e =>{
+                console.log("like doesnt exist")
+            })
+
     }, [])
 
+    useEffect(() => {
+        document.getElementById("post-num-likes").innerHTML = numLikes
+
+    }, [numLikes])
 
     function show() {
 
-        console.log("")
+        console.log("show")
+        console.log(post.likes)
+
         if (post === []) {
             return <Loading></Loading>
         } else {
+
+            let numLikes = post.numLikes;
             return (<div id="post-complete">
                 <div id="post-user-nickname">
                     <span>{post.ownerUser}</span>
@@ -36,10 +61,10 @@ export default function PostComplete() {
                     <img src={post.imageUrl} alt="" />
                 </div>
                 <div id="post-user-likes">
-                    <div>{post.numLikes} LIKES</div>
+                    <div ><span id="post-num-likes">{numLikes}</span> LIKES</div>
                     <div id="post-like-btn">
-                        <button onClick={() => {like()}}>
-                            dar like
+                        <button onClick={() => { like() }}>
+                            LIKE
                         </button>
                     </div>
                 </div>
@@ -52,23 +77,23 @@ export default function PostComplete() {
     }
 
     function like() {
-
-
         let token = localStorage.getItem("user-local-token");
-        api.post(`/posts/likes/${postId}`,{},
-        {headers : { "Authorization": `Bearer ${token}` }}).
+        api.post(`/posts/likes/${postId}`, {},
+            { headers: { "Authorization": `Bearer ${token}` } }).
             then(function (r) {
                 console.log(r);
                 let $btn = document.getElementById("post-like-btn")
                 if (r.data) {
                     $btn.style.backgroundColor = "blue"
-                }else{
+                    setNumLikes(numLikes + 1)
+                    console.log(numLikes, "----------")
+                } else {
                     $btn.style.backgroundColor = "transparent"
+                    setNumLikes(numLikes - 1)
                 }
             })
-                .catch(e => console.log(e))
+            .catch(e => console.log(e))
     }
-
 
     return (<div >
 
